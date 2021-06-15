682f0a3d2b02277d4b6cdb3184cfb55f40046cb8
1Soyebo <soyeboibukun@gmail.com>
2021-06-15 19:28:10 +0100

    added swift UIView

---
+++ FinalYearProject/Sidemenu/SettingsSwiftUI.swift
@@ -0,0 +1,46 @@
+//
+//  Settings.swift
+//  FinalYearProject
+//
+//  Created by Ibukunoluwa Soyebo on 15/06/2021.
+//
+
+import SwiftUI
+
+struct SettingsSwiftUI: View {
+    
+    @State var purchasedPower = ""
+    @State private var thresholdPower = ""
+    
+    var body: some View {
+      
+            Form{
+                Section(header: Text("Power Purchased in kwH")){
+                    TextField("How much power did you purchase", text: $purchasedPower)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section(header:Text("Threshold Power in kwH")){
+                    TextField("At what threshold  power do you want tot be notified to purchase power?", text: $thresholdPower)
+                        .keyboardType(.numberPad)
+                    
+                }
+            }        .navigationTitle("Settings")
+
+        
+        .onDisappear(perform: {
+            UserDefUtils.userPurchasedPower = Double(purchasedPower) ?? 0
+        })
+        
+        
+        
+    }
+    
+    
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsSwiftUI()
+    }
+}
