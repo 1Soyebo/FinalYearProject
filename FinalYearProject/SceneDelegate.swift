7fe58f06b6871399f8c8589af04ee16f75321ace
1Soyebo <soyeboibukun@gmail.com>
2021-07-12 19:12:09 +0100

    add badge number

--- FinalYearProject/SceneDelegate.swift
+++ FinalYearProject/SceneDelegate.swift
@@ -17,6 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        let hmmcenter = UNUserNotificationCenter.current()
+        UIApplication.shared.applicationIconBadgeNumber =  0
+        hmmcenter.removeAllDeliveredNotifications()
+        
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -62,10 +67,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
             let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
             center.add(request)
+            UIApplication.shared.applicationIconBadgeNumber += 1
+
         }
         
         
         center.add(generateDailyNotification())
+        UIApplication.shared.applicationIconBadgeNumber += 1
                 
     }
 
